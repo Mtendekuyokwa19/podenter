68d15d00d6b834434bd3a857e827574e8e7cd39d
Mtende Kuyokwa <mtendekuyokwa19@gmail.com>
2025-09-02 20:53:52 +0200

    setting up configs

--- app/about/layout.tsx
+++ app/about/layout.tsx
@@ -1,8 +1,13 @@
+import { PrismaClient } from "../../generated/prisma";
+const prisma = new PrismaClient();
 export default function AboutLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
+  async function stuff() {
+    await prisma.$connect();
+  }
   return (
     <section className="flex flex-col items-center justify-center gap-4 py-8 md:py-10">
       <div className="inline-block max-w-lg text-center justify-center">
